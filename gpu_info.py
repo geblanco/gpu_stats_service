7393c55cc8dbaa636643948b29f61cc432958032
Guillermo Blanco <guillermoechegoyenblanco@gmail.com>
2021-04-15 11:28:28 +0200

    Parse xml info with python script

---
+++ gpu_info.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import pwd
+import xml.etree.ElementTree as ET
+
+from datetime import datetime
+
+
+def get_pid_owner(pid):
+    # the /proc/PID is owned by process creator
+    proc_stat_file = os.stat(f"/proc/{pid}")
+    # get UID via stat call
+    uid = proc_stat_file.st_uid
+    # look up the username from uid
+    username = pwd.getpwuid(uid)[0]
+    return username
+
+
+def get_gpu_info(gpu_id, gpu_elem):
+    ret = None
+    proc_info = gpu_elem.find('processes').find('process_info')
+    if proc_info is not None:
+        pid = proc_info.find('pid').text
+        user = get_pid_owner(pid)
+        gpu_util_text = gpu_elem.find('utilization').find('gpu_util').text
+        mem = int(gpu_util_text.split(' ')[0])
+        ret = (user, pid, gpu_id, mem)
+    return ret
+
+
+# head: timestamp,user,pid,gpu,usage
+all_info = []
+empty_data = ('-', '-', '-', 0)
+timestamp = str(datetime.timestamp(
+    datetime.now().replace(second=0, microsecond=0)
+)).split('.')[0]
+tree = ET.parse(sys.stdin)
+root = tree.getroot()
+
+for gpu_id, gpu_elem in enumerate(root.findall('gpu')):
+    gpu_info = get_gpu_info(gpu_id, gpu_elem)
+    if gpu_info is not None:
+        all_info.append(gpu_info)
+
+if len(all_info) == 0:
+    all_info.append(empty_data)
+
+for info in all_info:
+    print(','.join([timestamp] + [str(i) for i in info]))
--- gpu_stats.sh
+++ gpu_stats.sh
@@ -4,21 +4,7 @@ log_dir="${HOME:-1}/gpu_stats"
 log_file="${log_dir}/$(date +%Y%m%d)_gpu_stats.csv"
 header="timestamp,user,pid,gpu,usage"
 
-print_gpu_stat(){
-    local timestamp=$(date +%s)
-    local nvidia_info=$(nvidia-smi -q -x)
-    local nof_gpus_inuse=$(echo "$nvidia_info" | grep pid | wc -l)
-    if [[ "$nof_gpus_inuse" -lt 1 ]]; then
-      echo "${timestamp} - - - 0" | column -t -o ","
-    else
-      local gpu_num_rows=$(seq 1 $nof_gpus_inuse)
-      local timerows=$(for i in `seq 1 $nof_gpus_inuse`; do echo $timestamp; done)
-      local user_pid_rows=$(echo "$nvidia_info" | grep pid | sed -e 's/<pid>//g' -e 's/<\/pid>//g' -e 's/^[[:space:]]*//' | xargs -n 1 ps --no-headers -up | awk '{print $1,$2}')
-      local gpu_util_rows=$(echo "$nvidia_info" | grep gpu_util | sed -e 's/<gpu_util>//g' -e 's/<\/gpu_util>//g' -e 's/^[[:space:]]*//' -e 's/\%//')
-      paste <(echo -n "$timerows") <(echo -n "$user_pid_rows") <(echo -n "$gpu_num_rows") <(echo -n "$gpu_util_rows") | column -t -o ","
-    fi
-}
-
 [[ ! -d "${log_dir}" ]] && mkdir -p "${log_dir}"
 [[ ! -f "${log_file}" ]] && echo "${header}" > "${log_file}"
-print_gpu_stat >> "${log_file}"
+nvidia-smi -x -q | python gpu_info.py 
+# nvidia-smi -x -q | python gpu_info.py >> "${log_file}"
